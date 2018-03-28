830635acc7d0efa23d8af3641184319a29ba522c
chinaniit <chinaniit@gmail.com>
2018-03-28 13:56:47 +0800

    Update buildsms.bat

--- buildsms.bat
+++ buildsms.bat
@@ -2,7 +2,7 @@
 @echo begin
 
 cd C:\Program Files (x86)\MSBuild\14.0\Bin\
-msbuild C:\_WorkAli_WIN10\SMS\Code\SMPMVC\Moon\Moon.Web.View.Sms\Moon.Web.View.Sms.csproj /p:Configuration=Debug
+msbuild C:\m.csproj /p:Configuration=Debug
 
 @echo end
 pause
