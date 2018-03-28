5cc66129227b6746d04613268db65014e0c46f6d
chinaniit <chinaniit@gmail.com>
2018-03-28 13:58:43 +0800

    Update getbuildm.bat

--- getbuildm.bat
+++ getbuildm.bat
@@ -1,12 +1,12 @@
 @echo off
 @echo begin get
 cd C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\
-TF.exe get C:\_WorkAli_WIN10\SMS\Code\SMPMVC\Moon /recursive
+TF.exe get C:\\M /recursive
 @echo end get
 
 @echo begin build
 cd C:\Program Files (x86)\MSBuild\14.0\Bin\
-msbuild C:\_WorkAli_WIN10\SMS\Code\SMPMVC\Moon\Moon.sln /p:Configuration=Debug /p:Platform="Any CPU"
+msbuild C:\M.sln /p:Configuration=Debug /p:Platform="Any CPU"
 @echo end build
 
 pause
