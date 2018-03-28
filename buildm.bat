ee85504972569bee4ba975b57d6a866746a83c1d
chinaniit <chinaniit@gmail.com>
2018-03-28 13:55:59 +0800

    Update buildm.bat

--- buildm.bat
+++ buildm.bat
@@ -2,7 +2,7 @@
 @echo begin
 
 cd C:\Program Files (x86)\MSBuild\14.0\Bin\
-msbuild C:\_WorkAli_WIN10\SMS\Code\SMPMVC\Moon\Moon.sln /p:Configuration=Debug /p:Platform="Any CPU"
+msbuild C:\M.sln /p:Configuration=Debug /p:Platform="Any CPU"
 
 @echo end
 pause
