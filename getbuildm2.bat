779e911af613494893523a5151852cd7298e3d9c
chinaniit <chinaniit@gmail.com>
2018-12-16 14:30:56 +0800

    Update getbuildm2.bat

--- getbuildm2.bat
+++ getbuildm2.bat
@@ -2,12 +2,12 @@
 
 @echo begin get
 cd C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\
-TF.exe get C:\_WorkAli_WIN10\SMS\Code\SMPMVC\M2 /recursive
+TF.exe get C:\M2 /recursive
 @echo end get
 
 @echo begin build
 cd C:\Program Files (x86)\MSBuild\14.0\Bin\
-msbuild C:\_WorkAli_WIN10\SMS\Code\SMPMVC\M2\M2.sln /p:Configuration=Release /p:Platform="Any CPU"
+msbuild C:\M2\M2.sln /p:Configuration=Release /p:Platform="Any CPU"
 @echo end build
 
 pause
