9821f711c4ef3efc18100f9a62040538d5eff02b
john L <chinaniit@gmail.com>
2022-10-10 10:49:58 +0800

    Update copym2.bat

--- copym2.bat
+++ copym2.bat
@@ -1,10 +1,10 @@
 @echo off
 @echo begin
 
-del C:\_WorkAli_WIN10\SMS\Code\SMPMVC\Moon\BusinessAPI.SDK\M2.*
-cd C:\_WorkAli_WIN10\SMS\Code\SMPMVC\M2\M2.Business\bin\Release\
-copy M2.*.dll C:\_WorkAli_WIN10\SMS\Code\SMPMVC\Moon\BusinessAPI.SDK\
-copy M2.*.pdb C:\_WorkAli_WIN10\SMS\Code\SMPMVC\Moon\BusinessAPI.SDK\
+del C:\_\BusinessAPI.SDK\M.*
+cd C:\
+copy M2.*.dll C:\SDK\
+copy M2.*.pdb C:\SDK\
 
 @echo end
 pause
